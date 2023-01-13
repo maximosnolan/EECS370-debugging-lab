911960b0b37dd8f810de8a6e3d031b9b96300c20
Maximos <maximosnolan@gmail.com>
2023-01-13 02:06:32 -0500

    updated code for lldb (mac users)

--- main.cc
+++ main.cc
@@ -12,7 +12,7 @@ namespace example {
     }
 
 //convert a vector of strings into a singular string seperated by spaces only IF all strings passed in are non empty
-    [[nodiscard]] static inline auto accumulate(std::vector<std::string> & vec) -> std::optional<std::string> {
+    [[nodiscard]] auto accumulate(std::vector<std::string> & vec) -> std::optional<std::string> {
         for (const auto & v : vec) {
             if (v == "") return std::nullopt;
         }
@@ -22,15 +22,15 @@ namespace example {
             return std::move(a) + " " + std::move(b);
         };
         std::string blank = "";
-        std::optional<std::string> returnValue = {std::accumulate(vec.begin(), vec.end(), blank,  add_space_fold)};
-        //See what the return value is!
+        auto returnValue = std::accumulate(vec.begin(), vec.end(), blank,  add_space_fold);
+    //     //See what the return value is!
 
-        return returnValue;
+    return {returnValue};
     }
 
 }
 
-[[nodiscard]] auto main(int16_t argc, char ** argv) -> int {
+[[nodiscard]] int main(int argc, char ** argv) {
     //debugger step 1!
     printf("1: Hello world!\n");
     int32_t capturedReturnValue = 4;
@@ -44,11 +44,11 @@ namespace example {
     //sometimes you might jump into a function that makes no sense (our starter code or a library function)
     std::vector<std::string> vec= {"Hello", "World", "This","Is","How","You","Can","Use","Modern","C++","Effectivly"};
 
-    auto value = example::accumulate(vec);
-    if (value.has_value()) {
-        std::cout << "Here is the value we got! " << *value << "\n";
-    }
+    std::optional<std::string> value = example::accumulate(vec);
+    // if (value.has_value()) {
+    //     std::cout << "Here is the value we got! " << *value << "\n";
+    // }
     std::cout << "Notice how sometimes it's best to not jump into code we didn't write, we can just step over those functions :) \n";
-
-
+    return 0;
 }
+
